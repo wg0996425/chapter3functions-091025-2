2b87a9fe6d4d32fac6ccda1526399ac1975afac8
Viktor <vg0996425@otc.edu>
2025-09-06 21:06:33 -0500

    made original. might add bootstrap

--- app.php
+++ app.php
@@ -1,14 +1,17 @@
 <?php
     declare(strict_types = 1);
-    $candy = [
-        'Toffee' => ['price' => 3.00, 'stock' => 12],
-        'Mints'  => ['price' => 2.00, 'stock' => 26],
-        'Fudge' => ['price' => 4.00, 'stock' => 8],
+    $book = [
+        'The Joy Luck Club'   => ['author' => 'Amy Tan', 'price' => 13.00, 'stock' => 1],
+        'Pride and Predudice' => ['author' => 'Jane Austin', 'price' => 12.00, 'stock' => 6],
+        'American Psycho'     => ['author' => 'Bret Easton Elis', 'price' => 20.00, 'stock' => 8],
+        'The Federalist'      => ['author' => 'Alexander Hamilton', 'price' => 14.00, 'stock' => 2],
+        'Scythe'              => ['author' => 'Neil Shusterman', 'price' => 16.00, 'stock' => 1],
+        'Fahrenheit 451'      => ['author' => 'Ray Bradbury', 'price' => 10.00, 'stock' => 7],
     ];
-    $tax = 20;
+    $tax = 5;
 
     function get_reorder_message(int $stock): string {
-        return ($stock < 10) ? 'Yes' : 'No';
+        return ($stock < 3) ? 'Yes' : 'No';
     }
 
     function get_total_value(float $price, int $quantity): float {
@@ -25,18 +28,19 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>The Candy Store</title>
+        <title>Book Store</title>
     </head>
     <body>
-        <h1>The Candy Store</h1>
+        <h1>Book Store</h1>
         <h2>Stock Control</h2>
         <table>
             <tr>
-                <th>Candy</th><th>Stock</th><th>Re-order</th><th>Total value</th><th>Tax due</th>
+                <th>Title</th><th>Author</th><th>Stock</th><th>Re-order</th><th>Total value</th><th>Tax due</th>
             </tr>
-            <?php foreach ($candy as $product_name => $data) { ?>
+            <?php foreach ($book as $product_name => $data) { ?>
                 <tr>
                     <td><?= $product_name ?></td>
+                    <td><?= $data['author'] ?></td>
                     <td><?= $data['stock'] ?></td>
                     <td><?= get_reorder_message($data['stock']) ?></td>
                     <td>$<?= get_total_value($data['price'], $data['stock']) ?></td>
